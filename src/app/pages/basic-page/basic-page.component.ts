8312fe01bcb7baaeb3d7caddae6dccd8e5d58521
williamhdz94 <williamhernandez450@gmail.com>
2025-04-25 07:24:29 -0600

    Text pipes and number pipes

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.component.html',
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+
+  nameLower = signal('william');
+  nameUpper = signal('WILLIAM');
+  fullName = signal('wIlLiAn HernAnDez');
+
+}
--- src/app/pages/number-page/number-page.component.ts
+++ src/app/pages/number-page/number-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [
+    DecimalPipe,
+    PercentPipe,
+    CurrencyPipe
+  ],
   templateUrl: './number-page.component.html',
 })
-export default class NumberPageComponent { }
+export default class NumberPageComponent {
+
+  totalSells = signal(2_433_232.5567);
+  percent = signal(0.2366)
+
+}
