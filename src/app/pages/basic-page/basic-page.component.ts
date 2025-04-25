68cf0a0a8a0a4d10a3b40fc8bb3ec2e9647b4a42
williamhdz94 <williamhernandez450@gmail.com>
2025-04-25 07:42:09 -0600

    date pipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,13 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe
   ],
   templateUrl: './basic-page.component.html',
 })
@@ -15,5 +16,19 @@ export default class BasicPageComponent {
   nameLower = signal('william');
   nameUpper = signal('WILLIAM');
   fullName = signal('wIlLiAn HernAnDez');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+
+  });
 
 }
