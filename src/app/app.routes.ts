e053dd5d71d3a6fbc8ef60c4a6beb86be0cc8f24
williamhdz94 <williamhernandez450@gmail.com>
2025-04-25 06:58:42 -0600

    Add structure, routes and reading routes from navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component')
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipe',
+    loadComponent: () => import('./pages/number-page/number-page.component')
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component')
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+];
---
+++ src/app/pages/number-page/number-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-number-page',
+  imports: [],
+  templateUrl: './number-page.component.html',
+})
+export default class NumberPageComponent { }
