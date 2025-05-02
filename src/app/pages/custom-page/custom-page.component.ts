e58a197f9cc55713f3336caf28f4011a7ca48fcd
williamhdz94 <williamhernandez450@gmail.com>
2025-05-02 07:00:50 -0600

    Custom pipes [Filter in array, Filter by option]

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,10 +6,13 @@ import { HeroColorPipe } from '../../pipes/heroColor.pipe';
 import { HeroTextColorPipe } from '../../pipes/heroTextColor.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/heroCreator.pipe';
+import { HeroSortByPipe } from '../../pipes/heroSortBy.pipe';
+import { IHero } from '../../interfaces/IHero';
+import { HeroFilterPipe } from '../../pipes/heroFilter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [TogglePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
+  imports: [TogglePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -17,6 +20,8 @@ export default class CustomPageComponent {
   name = signal('William Hernandez');
   upperCase = signal(true);
   heroes = signal(heroes);
+  sortBy = signal<keyof IHero | null>(null);
+  searchQuery = signal('');
 
   toggle() {
     this.upperCase.set(!this.upperCase());
